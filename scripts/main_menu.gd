76394e3c23d6b81fa74de050f99dcb077084a1e0
Cody Jones <cjonesfatloaf@gmail.com>
2025-05-19 22:14:52 -0400

    made main menu work with controller

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,3 +5,27 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://worlds/stage_select.tscn")
 	var stream_playback : AudioStreamPlaybackInteractive = MusicPlayer.get_stream_playback()
 	stream_playback.switch_to_clip_by_name(&"Stall Battle")
+
+
+func _ready():
+	$start.grab_focus()
+
+
+func _input(event):
+	var current = get_viewport().gui_get_focus_owner()
+	if not current: #should not happen but in case it does
+		return
+
+	if event is InputEventJoypadMotion:
+		if event.axis == JOY_AXIS_LEFT_Y: #vertical left stick
+			if event.axis_value == -1.0: #full motion up
+				var prev = get_node(current.focus_previous)
+				prev.grab_focus()
+			elif event.axis_value == 1.0: # full motion down
+				var next = get_node(current.focus_next)
+				next.grab_focus()
+
+	if event is InputEventJoypadButton:
+		if event.button_index == JOY_BUTTON_A and event.pressed:
+			if current is Button:
+				current.pressed.emit()
