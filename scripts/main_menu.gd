cceac5b0b20a36f29232703e2b19d64d11395e6c
Cody Jones <cjonesfatloaf@gmail.com>
2025-05-21 18:37:49 -0400

    made main menu usable with controller

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -13,8 +13,7 @@ func _ready():
 
 func _input(event):
 	var current = get_viewport().gui_get_focus_owner()
-	if not current: #should not happen but in case it does
-		return
+	if not current: return
 
 	if event is InputEventJoypadMotion:
 		if event.axis == JOY_AXIS_LEFT_Y: #vertical left stick
@@ -24,8 +23,7 @@ func _input(event):
 			elif event.axis_value == 1.0: # full motion down
 				var next = get_node(current.focus_next)
 				next.grab_focus()
-
-	if event is InputEventJoypadButton:
+	elif event is InputEventJoypadButton:
 		if event.button_index == JOY_BUTTON_A and event.pressed:
 			if current is Button:
 				current.pressed.emit()
