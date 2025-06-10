137702ddb4384df586b7017ed181ea2b577f568c
Cody Jones <cjonesfatloaf@gmail.com>
2025-06-10 10:37:20 -0400

    fixed player sprites not updating in lobby

--- scripts/lobby_menu.gd
+++ scripts/lobby_menu.gd
@@ -1,12 +1,13 @@
 extends Control
 
+@export var player_sprites : Array[Sprite2D]
 
 func _ready():
 	Lobby.player_connected.connect(_on_player_connected)
 
 
 func _on_player_connected(peer_id, player_info):
-	$player2.show()
+	player_sprites[1].show()
 	if is_multiplayer_authority():
 		$start.disabled = false
 	else:
