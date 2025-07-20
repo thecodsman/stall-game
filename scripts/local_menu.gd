3ed21e9b2e0032538695a03dde6f0cd09f6b5ccd
Cody Jones <cjonesfatloaf@gmail.com>
2025-07-19 21:47:56 -0400

    made menus more navigateable with controller

--- scripts/local_menu.gd
+++ scripts/local_menu.gd
@@ -4,12 +4,16 @@ extends Control
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event is InputEventJoypadButton:
-		if Globals.registered_controllers.has(event.device) || Globals.registered_controllers.size() >= player_sprites.size(): return
-		player_sprites[Globals.registered_controllers.size()].show()
-		Globals.registered_controllers.append(event.device)
-		if Globals.registered_controllers.size() > 1:
-			$start.disabled = false
+	if not event is InputEventJoypadButton: return
+	match event.button_index:
+		JOY_BUTTON_START:
+			$start.pressed.emit()
+		_:
+			if Globals.registered_controllers.has(event.device) || Globals.registered_controllers.size() >= player_sprites.size(): return
+			player_sprites[Globals.registered_controllers.size()].show()
+			Globals.registered_controllers.append(event.device)
+			if Globals.registered_controllers.size() > 1:
+				$start.disabled = false
 		
 
 func _on_start_pressed() -> void:
